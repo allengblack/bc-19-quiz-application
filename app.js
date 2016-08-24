bc53d05896c751e1fe8484418b3115fafade22c5
allengblack <allengblack@gmail.com>
2016-08-24 14:31:00 +0100

    developed quiz models for landing page

--- app.js
+++ app.js
@@ -2,27 +2,26 @@
  * Created by g-man on 8/24/16.
  */
 
-function errorHandler (err, obj) {
+
+function errorhandler (err, obj) {
     //a global error handler
     console.error(err, obj);
 }
 
-var app = angular.module("quiz-app", [ui.bootstrap]);
+var app = angular.module("quiz-app", []);
 
 app.controller("QuizCtrl", function ($scope, $sce, $http) {
     $scope.quizzes = [];
 
-    $scope.selectQuiz = function (index) {
-        $scope.selectedQuiz = $scope.quizzes[index];
-
+    $scope.selectQuiz = function () {
         //get the json data from the selected quiz url
         $http.get($scope.selectedQuiz.dataUrl).success(function (response) {
             $scope.quiz = response;
             console.log(response);
         }).error(errorhandler);
-    }
+    };
 
-    $http.get("quiz_lib/quiz-choices.json").success(function (response) {
+    $http.get("quiz_library/quiz_choices.json").success(function (response) {
         $scope.quizzes = response;
         $scope.selectedQuiz = $scope.quizzes[0];
         console.log(response);
