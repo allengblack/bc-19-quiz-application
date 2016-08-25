7fb25dd51a70293ae4a4a1423dd89c3e05cd9222
allengblack <allengblack@gmail.com>
2016-08-25 08:16:46 +0100

    Added ng-app ID to the html tag which fixed an incorrect display problem

--- app.js
+++ app.js
@@ -1,50 +1,77 @@
-/**
- * Created by g-man on 8/24/16.
- */
 function errorhandler (err, obj) {
     //a global error handler
     console.error(err, obj);
 }
 
-var app = angular.module("quiz-app", ['ngRoute']);
+var scotchApp = angular.module('scotchApp', []);
 
-app.controller('MainCtrl', function ($scope) {
-    $scope.message = "Hello, welcome to the Quiz App.";
-});
-
-app.controller('MainCtrl', function ($scope) {
-    $scope.message = "Hello, welcome to the Quiz App.";
+scotchApp.controller('MainCtrl', function($scope) {
+    $scope.message = "Welcome to the Quiz App!";
 });
 
 
-app.controller("QuizCtrl", function ($scope, $sce, $http) {
-    $scope.quizzes = [];
 
-    $scope.selectQuiz = function () {
-        //get the json data from the selected quiz url
-        $http.get($scope.selectedQuiz.dataUrl).success(function (response) {
-            $scope.quiz = response;
-            console.log(response);
-        }).error(errorhandler);
-    };
 
-    $http.get("quiz_choices.json").success(function (response) {
-        $scope.quizzes = response;
-        // $scope.selectedQuiz = $scope.quizzes[0];
-        console.log(response);
-    }).error(errorhandler);
 
-});
 
-app.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl : 'index.html',
-            controller : 'MainCtrl'
-        })
 
-        .when('/takeQuiz', {
-            templateUrl : 'quiz.html',
-            controller : 'QuizCtrl'
-        })
-});
+
+
+
+// /**
+//  * Created by g-man on 8/24/16.
+//  */
+// function errorhandler (err, obj) {
+//     //a global error handler
+//     console.error(err, obj);
+// }
+//
+// var app = angular.module("quiz-app", ['ngRoute']);
+//
+// app.controller('MainCtrl', function($scope) {
+//     $scope.message = "Hello, welcome to the Quiz App.";
+//
+// });
+//
+// app.controller('UploadCtrl', function($scope) {
+//     $scope.message = "Hello, welcome to the Quiz App.";
+//
+// });
+//
+// app.controller("QuizCtrl", function($scope, $sce, $http) {
+//     $scope.quizzes = [];
+//     $scope.message = "Select one of the quizzes.";
+//
+//     $scope.selectQuiz = function () {
+//         //get the json data from the selected quiz url
+//         $http.get($scope.selectedQuiz.dataUrl).success(function (response) {
+//             $scope.quiz = response;
+//             console.log(response);
+//         }).error(errorhandler);
+//     };
+//
+//     $http.get("quiz_choices.json").success(function (response) {
+//         $scope.quizzes = response;
+//         // $scope.selectedQuiz = $scope.quizzes[0];
+//         console.log(response);
+//     }).error(errorhandler);
+//
+// });
+//
+// app.config(function($routeProvider) {
+//     $routeProvider
+//         .when('/', {
+//             templateUrl : 'templates/home.html',
+//             controller : 'MainCtrl'
+//         })
+//
+//         .when('/takeQuiz', {
+//             templateUrl : 'TakeQuiz.html',
+//             controller : 'QuizCtrl'
+//         })
+//
+//         .when('/uploadQuiz', {
+//             templateUrl : 'uploadQuiz.html',
+//             controller : 'UploadCtrl'
+//         })
+// });
