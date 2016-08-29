9927725c6b20a98e8a9aae672fd21ea131b55610
allengblack <allengblack@gmail.com>
2016-08-29 10:15:19 +0100

    new

--- controllers/UploadCtrl.js
+++ controllers/UploadCtrl.js
@@ -4,16 +4,5 @@
 
 
 quizApp.controller('UploadCtrl', function($scope) {
-
     $scope.templateUrl = '/quiz_files/template.json';
-    //
-    // $scope.add = function () {
-    //     var f = document.getElementById('file').files[0],
-    //         r = new FileReader();
-    //     r.onloadend = function (e) {
-    //         var data = e.target.result;
-    //     };
-    //     r.readAsBinaryString(f);
-    // };
-
 });
